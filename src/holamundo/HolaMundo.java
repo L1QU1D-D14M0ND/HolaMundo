aa417a1fe4f3b114cc009e196af34f68267a2b42
L1QU1D-D14M0ND <tomrod04@gmail.com>
2024-11-11 19:37:19 +0100

    Version 1.1

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -12,6 +12,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hola mundo. ");
+        System.out.print("Segunda version del proyecto en git. ");
     }
     
 }
